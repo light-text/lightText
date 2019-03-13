521ebf29bc04b7e27454d8ebcb8d66ada1b22718
Maurice Shalam <mss619@nyu.edu>
2019-03-13 15:21:20 -0400

    setup basic twilio functionality

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/sms', require('./receive_sms'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+
 module.exports = router
 
 router.get('/', async (req, res, next) => {
